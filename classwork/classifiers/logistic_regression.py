49ad35c4e0167b4285a443a1ccc982fd6857572b
qiushibo <1114676346@qq.com>
2022-06-28 19:01:13 +0800

    commit logistic_regression.py

--- classwork/classifiers/logistic_regression.py
+++ classwork/classifiers/logistic_regression.py
@@ -1,165 +1,56 @@
 import numpy as np
-import random
-import math
-
-from bitstring import xrange
 
 
-class LogisticRegression(object):
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
 
-    def __init__(self):
-        self.w = None
-        self.ww = None
 
-    def sigmoid(self, z):
-        return 1.0 / (1.0 + np.exp(-z))
+def costFunction(theta, X, y):
+    m = X.shape[0]
+    0
+    z = X.dot(theta)
+    J = (-y.T.dot(np.log(sigmoid(z))) - (1 - y.T).dot(np.log(1 - sigmoid(z)))) / m
+    return J[0]
 
-    def loss(self, X_batch, y_batch):
-        """
-        Compute the loss function and its derivative.
-        Subclasses will override this.
 
-        Inputs:
-        - X_batch: A numpy array of shape (N, D) containing a minibatch of N
-        data points; each point has dimension D.
-        - y_batch: A numpy array of shape (N,) containing labels for the minibatch.
+def gradient(theta, X, y):
+    m = X.shape[0]  # X.shape = (100,3)
+    grad = np.zeros_like(theta)
+    z = X.dot(theta.reshape(-1, 1))  # theta.shape = (3,) theta.reshape(-1,1).shape = (3,1)
+    grad = (1.0 / m) * X.T.dot((sigmoid(z) - y))
+    return grad.flatten()
 
-        Returns: A tuple containing:
-        - loss as a single float
-        - gradient with respect to self.W; an array of the same shape as W
-        """
-        #########################################################################
-        # TODO:                                                                 #
-        # calculate the loss and the derivative                                 #
-        #########################################################################
-        m = X_batch.shape[0]
-        loss = 0
-        z = X_batch.dot(self.w)
-        loss = (-y_batch.T.dot(np.log(self.sigmoid(z))) - (1.0 - y_batch.T).dot(np.log(1.0 - self.sigmoid(z)))) / m
-        grad = np.zeros_like(self.w)
-        grad = X_batch.T.dot(self.sigmoid(z) - y_batch) / m
-        #########################################################################
-        #                       END OF YOUR CODE                                #
-        #########################################################################
-        return loss, grad
 
-    def train(self, X, y, learning_rate=1e-3, num_iters=100,
-              batch_size=200, verbose=True):
+# def mapFeature(X):
+#     poly = PolynomialFeatures(6)
+#     XX = poly.fit_transform(X)
+#     return XX
 
-        """
-        Train this linear classifier using stochastic gradient descent.
-        Inputs:
-        - X: A numpy array of shape (N, D) containing training data; there are N
-         training samples each of dimension D.
-        - y: A numpy array of shape (N,) containing training labels;
-        - learning_rate: (float) learning rate for optimization.
-        - num_iters: (integer) number of steps to take when optimizing
-        - batch_size: (integer) number of training examples to use at each step.
-        - verbose: (boolean) If true, print progress during optimization.
+def mapFeature(x1col, x2col):
+    degrees = 6
+    out = np.ones((x1col.shape[0], 1))
+    for i in range(1, degrees + 1):
+        for j in range(0, i + 1):
+            term1 = x1col ** (i - j)
+            term2 = x2col ** (j)
+            term = (term1 * term2).reshape(term1.shape[0], 1)
+            out = np.hstack((out, term))
+    return out
 
-        Outputs:
-        A list containing the value of the loss function at each training iteration.
-        """
-        num_train, dim = X.shape
-        if self.w is None:
-            self.w = 0.001 * np.random.randn(dim)
-        loss_history = []
-        for it in xrange(num_iters):
-            X_batch = None
-            y_batch = None
-            #########################################################################
-            # TODO:                                                                 #
-            # Sample batch_size elements from the training data and their           #
-            # corresponding labels to use in this round of gradient descent.        #
-            # Store the data in X_batch and their corresponding labels in           #
-            # y_batch; after sampling X_batch should have shape (batch_size, dim)   #
-            # and y_batch should have shape (batch_size,)                           #
-            #                                                                       #
-            # Hint: Use np.random.choice to generate indices. Sampling with         #
-            # replacement is faster than sampling without replacement.              #
-            #########################################################################
-            Sample_batch = np.random.choice(np.arange(num_train), batch_size)
-            X_batch = X[Sample_batch]
-            y_batch = y[Sample_batch]
-            #########################################################################
-            #                       END OF YOUR CODE                                #
-            #########################################################################
-            # evaluate loss and gradient
-            loss, grad = self.loss(X_batch, y_batch)
-            loss_history.append(loss)
-            # perform parameter update
-            #########################################################################
-            # TODO:                                                                 #
-            # Update the weights using the gradient and the learning rate.          #
-            #########################################################################
-            self.w += -learning_rate * grad
-            #########################################################################
-            #                       END OF YOUR CODE                                #
-            #########################################################################
-            if verbose and it % 100 == 0:
-                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
-        return loss_history
+def costFunctionReg(theta, lamb, X, y):
+    m = X.shape[0]
+    J = 0
+    z = X.dot(theta)
+    J = (-y.T.dot(np.log(sigmoid(z))) - (1 - y.T).dot(np.log(1.0 - sigmoid(z)))) / m
+    J += lamb / (2.0 * m) * theta[1:].T.dot(theta[1:])
+    return J[0]
 
-    def predict(self, X):
-        """
-        Use the trained weights of this linear classifier to predict labels for
-        data points.
 
-        Inputs:
-        - X: D x N array of training data. Each column is a D-dimensional point.
-
-        Returns:
-        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-        array of length N, and each element is an integer giving the predicted
-        class.
-        """
-        y_pred = np.zeros(X.shape[0])
-        ###########################################################################
-        # TODO:                                                                   #
-        # Implement this method. Store the predicted labels in y_pred.            #
-        ###########################################################################
-        y_pred = self.sigmoid(X.dot(self.w))
-        for i in range(X.shape[0]):
-            if y_pred[i] >= 0.50:
-                y_pred[i] = 1
-            else:
-                y_pred[i] = 0
-        ###########################################################################
-        #                           END OF YOUR CODE                              #
-        ###########################################################################
-        return y_pred
-
-    def one_vs_all(self, X, y, learning_rate=1e-3, num_iters=100,
-                   batch_size=200, verbose=True):
-        """
-        Train this linear classifier using stochastic gradient descent.
-        Inputs:
-        - X: A numpy array of shape (N, D) containing training data; there are N
-         training samples each of dimension D.
-        - y: A numpy array of shape (N,) containing training labels;
-        - learning_rate: (float) learning rate for optimization.
-        - num_iters: (integer) number of steps to take when optimizing
-        - batch_size: (integer) number of training examples to use at each step.
-        - verbose: (boolean) If true, print progress during optimization.
-        """
-
-        num_train, dim = X.shape
-        self.ww = np.zeros((dim, 10))
-        for it in range(10):
-            y_train = []
-            for label in y:
-                if label == it:
-                    y_train.append(1)
-                else:
-                    y_train.append(0)
-            y_train = np.array(y_train)
-            self.w = None
-            print("it = ", it)
-            self.train(X, y_train, learning_rate, num_iters, batch_size)
-            self.ww[:, it] = self.w
-
-    def one_vs_all_predict(self, X):
-        laybels = self.sigmoid(X.dot(self.ww))
-        y_pred = np.argmax(laybels, axis=1)
-        return y_pred
+def gradientReg(theta, lamb, X, y):
+    m = X.shape[0]
+    np.zeros_like(theta)
+    z = X.dot(theta.reshape(-1, 1))
+    grad = (1.0 / m) * X.T.dot((sigmoid(z) - y)) + (lamb / m) * theta.reshape(-1, 1)
+    grad[0] -= (lamb / m) * theta[0]
+    return grad.flatten()
