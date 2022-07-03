4ebda871224fee1d4ff325f711aa6b46986623db
qiushibo <1114676346@qq.com>
2022-07-03 19:59:44 +0800

    优化若干

--- optimizeMinimize/classifier/logistic_regression.py
+++ optimizeMinimize/classifier/logistic_regression.py
@@ -1,3 +1,4 @@
+import locale
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -16,7 +17,7 @@ class LogisticRegression(object):
         return J[0]
 
     def gradient(self, theta, X, y): 
-        m = X.shape[0] #X.shape = (100,3)
+        m = X.shape[0] #X.shape (100,3)
         grad = np.zeros_like(theta)
         z = X.dot(theta.reshape(-1,1)) # theta.shape = (3,) theta.reshape(-1,1).shape = (3,1)
         grad = (1.0 / m) * X.T.dot(( self.sigmoid(z) - y ))  
@@ -39,7 +40,7 @@ class LogisticRegression(object):
         J = 0
         z = X.dot(theta)
         J = (-y.T.dot(np.log(self.sigmoid(z))) - (1 - y.T).dot(np.log(1.0 - self.sigmoid(z)))) / m
-        J += lamb/(2.0*m) * theta[1:].T.dot(theta[1:])
+        J += lamb/(2.0*m) * theta[1:].T.dot(theta[1:]) # 公式换算，返回第一个属性值
         return J[0]
 
     def gradientReg(self, theta, lamb, X, y):
@@ -53,28 +54,29 @@ class LogisticRegression(object):
 
 
     def loadData(self, path):
-        path = os.getcwd() + path
+        path = os.getcwd() + path # 获取到当前工作目录
         data = np.loadtxt(path, dtype=float, delimiter=',')
-        X = data[:, 0:2]
-        y = data[:, 2]
+        X = data[:, 0:2] # 点 根据数据集的不同而不同
+        y = data[:, 2] # 已经分类好的label
         return X, y
 
     def plotScatter(self, path):
         X, y = self.loadData(path)
-        y = y.astype(int)
+        y = y.astype(int) # 确保label是int型的0或1
         neg = (y == 0)
         pos = (y == 1)
-        lable1 = plt.scatter(X[neg, 0], X[neg, 1], marker='o', c='b')
-        lable2 = plt.scatter(X[pos, 0], X[pos, 1], marker='o', c='r')
-        plt.legend((lable1, lable2), ('Admitted', 'Not admitted'))
-        plt.xlabel('Exam 1 score')
-        plt.ylabel('Exam 2 score')
+        print(type(neg))
+        lable1 = plt.scatter(X[neg, 0], X[neg, 1], marker='^', c='g', alpha=0.7) # 绘制散点图，先通过上面的两行分类成neg和pos，然后再
+        lable2 = plt.scatter(X[pos, 0], X[pos, 1], marker='v', c='r', alpha=0.7)
+        plt.legend((lable1, lable2), ('pass', 'not pass'))
+        plt.xlabel('exam 1')
+        plt.ylabel('exam 2')
 
     def decisionBoundary(self, theta, path):
         self.plotScatter(path)
         X, y = self.loadData(path)
         y.astype(int)
-        boundary_xs = np.array([np.min(X[:, 0]), np.max(X[:, 0])])
+        boundary_xs = np.array([np.min(X[:, 0]), np.max(X[:, 0])]) # 该参数为第一列数据的[最小值,最大值]的数组
         boundary_ys = (-1. / theta[2]) * (theta[0] + theta[1] * boundary_xs)
         plt.plot(boundary_xs, boundary_ys)
 
@@ -113,16 +115,6 @@ class LogisticRegression(object):
         self.plotScatter(path)
         self.decisionBoundaryReg(theta, X, y, 100.)
 
-    def Logistic(self, path):
-        X, y = self.loadData(path)
-        m, n = X.shape
-        X = np.c_[np.ones((m, 1)), X]  # add one col
-        y = np.c_[y]
-        initial_theta = np.zeros(X.shape[1])
-        res = op.minimize(self.costFunction, initial_theta, args=(X, y), method='TNC', jac=self.gradient, options={'maxiter': 400})
-        theta = res.x
-        self.decisionBoundary(theta, path)
-        plt.show()
 
     def LogisticReg(self, path):
         X, y = self.loadData(path)
