500e1644f878a45f538459def5ab79dff808be51
qiushibo <1114676346@qq.com>
2022-07-07 08:23:53 +0800

    完成

--- classwork/classifiers/logistic_regression.py
+++ classwork/classifiers/logistic_regression.py
@@ -13,90 +13,53 @@ class LogisticRegression(object):
         return 1.0 / ( 1.0 + np.exp(-z) )
 
     def loss(self, X_batch, y_batch):
-        """
-        计算损失函数及其导数。
-        子类将覆盖它。
-        输入:
-        - X_batch:形状为(N,D)的numpy数组,包含N的minibatch
-        数据点;每个点都有维数d。
-        - y_batch:shape(N,)的numpy数组,包含minibatch的标签。
-        返回:一个元组,包含:
-        -作为单一浮动的损失
-        -相对于自身的梯度。w;与W形状相同的数组
-        """
+        
+        m = X_batch.shape[0] 
 
-        # 计算损失和导数
-        m = X_batch.shape[0]
         loss = 0
-        z = X_batch.dot(self.w)
-        loss = (-y_batch.T.dot(np.log(self.sigmoid(z))) - (1.0 - y_batch.T).dot(np.log(1.0 - self.sigmoid(z))))/m
-        grad = np.zeros_like(self.w)
-        grad = X_batch.T.dot(self.sigmoid(z) - y_batch) /m
+
+        z = X_batch.dot(self.w) 
+
+        loss = (-y_batch.T.dot(np.log(self.sigmoid(z))) - (1.0 - y_batch.T).dot(np.log(1.0 - self.sigmoid(z))))/m 
+
+        grad = np.zeros_like(self.w) 
+        grad = X_batch.T.dot(self.sigmoid(z) - y_batch) /m 
 
         return loss,grad
+
     def train(self, X, y, learning_rate=1e-3, num_iters=100,
             batch_size=200, verbose=True):
+        
+        num_train, dim = X.shape 
 
-        """
-        使用随机梯度下降训练这个线性分类器。
-        Inputs:
-        - X:包含训练数据的shape (N,D)的numpy数组;有N个d维的训练样本。
-        - y:包含训练标签的shape (N,)的numpy数组;
-        - learning_rate: (float)优化的学习率。
-        - num_iters: (integer)优化时要采取的步骤数
-        - batch_size:(整数)每个步骤中要使用的训练示例的数量。
-        - verbose: (boolean)如果为true,则在优化过程中打印进度。
-        产出:
-        包含每次训练迭代的损失函数值的列表。
-        """
-        num_train, dim = X.shape
         if self.w is None:
             self.w = 0.001 * np.random.randn(dim)
+
         loss_history = []
+
         for it in xrange(num_iters):
             X_batch = None
             y_batch = None
-            '''
-            # TODO: #
-            #来自训练数据的样本batch_size元素及其#
-            #在这一轮梯度下降中使用的相应标签。#
-            #将数据存储在X_batch中.并将它们对应的标签存储在#
-            # y _ batch采样后.X_batch应具有形状(batch_size.dim) #
-            #和y_batch应具有shape (batch_size.)#
-            #提示:使用np.random.choice生成索引。用#采样
-            #替换比不替换的采样更快。
-            '''
-            # print("batchSize：",batch_size)
+
             Sample_batch = np.random.choice(np.arange(num_train), batch_size)
+
             X_batch = X[Sample_batch]
             y_batch = y[Sample_batch]
 
-            # evaluate loss and gradient
             loss, grad = self.loss(X_batch, y_batch)
             loss_history.append(loss)
 
-            # perform parameter update
-            #########################################################################
-            # TODO:                                                                 #
-            # 使用梯度和学习率更新权重。         
-            #########################################################################
             self.w += -learning_rate*grad
-            #########################################################################
-            #                       END OF YOUR CODE                                #
-            #########################################################################
+
             if verbose and it % 50 == 0:
                 print ('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
         return loss_history
+
+
+
     def predict(self, X):
-        """
-            使用此线性分类器的训练权重来预测数据点的标签。
-            输入:
-            训练数据的X: D x N数组。每一列都是一个D维点。
-            返回
-            -y_pred:x中数据的预测标签。y _ pred是一维的
-            长度为N的数组,每个元素都是给出预测类的整数。
-        """
+
         y_pred = np.zeros(X.shape[0])
 
         # 实现此方法。将预测的标签存储在y_pred中。           
@@ -112,21 +75,14 @@ class LogisticRegression(object):
 
     def one_vs_all(self, X, y, learning_rate=1e-3, num_iters=100,
                    batch_size=200, verbose=True):
-    #     """
-    #     使用随机梯度下降训练这个线性分类器。
-    #     输入:
-    #     - X:包含训练数据的shape (N,D)的numpy数组;有N个
-    #     d维的训练样本。
-    #     - y:包含训练标签的shape (N,)的numpy数组;
-    #     - learning_rate: (float)优化的学习率。
-    #     - num_iters: (integer)优化时要采取的步骤数
-    #     - batch_size:(整数)每个步骤中要使用的训练示例的数量。
-    #     - verbose: (boolean)如果为true,则在优化过程中打印进度。
-    #     """
-        loss_history_one = np.empty(shape=[1,num_iters],dtype=float)
+
+        loss_history_one = np.empty(shape=[1,num_iters],dtype=float)# [一行，学习步骤次数 列]
         loss_history_servel = np.empty(shape=[10,num_iters],dtype=float)
+
         num_train, dim = X.shape
+
         self.ww = np.zeros((dim, 10))
+
         for it in range(10):
             y_train = []
             for label in y:
@@ -134,6 +90,7 @@ class LogisticRegression(object):
                     y_train.append(1)
                 else:
                     y_train.append(0)
+
             y_train = np.array(y_train)
             self.w = None
             print("本次训练的数字是 = ", it)
--- optimizeMinimize/classifier/optimize_minimize.py
+++ optimizeMinimize/classifier/optimize_minimize.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 import os
 import scipy.optimize as op
 
-class LogisticRegression(object):
+class OptimizeMinmize(object):
 
     def sigmoid(self, z):
         return 1.0 / (1.0 + np.exp(-z))
 
+
     def costFunction(self, theta, X, y):
         m = X.shape[0]
-        J = 0
+
         z = X.dot(theta)
+
         J = (-y.T.dot(np.log(self.sigmoid(z))) - (1 - y.T).dot(np.log(1 - self.sigmoid(z)))) / m
         return J[0]
 
     def gradient(self, theta, X, y): 
-        m = X.shape[0] #X.shape (100,3)
+        m = X.shape[0] 
         grad = np.zeros_like(theta)
-        z = X.dot(theta.reshape(-1,1)) # theta.shape = (3,) theta.reshape(-1,1).shape = (3,1)
+        z = X.dot(theta.reshape(-1,1))
         grad = (1.0 / m) * X.T.dot(( self.sigmoid(z) - y ))  
         return grad.flatten()
 
@@ -35,38 +37,42 @@ class LogisticRegression(object):
                 out   = np.hstack(( out, term ))
         return out
 
+
     def costFunctionReg(self, theta, lamb, X, y):
+
         m = X.shape[0]
-        J = 0
         z = X.dot(theta)
+
         J = (-y.T.dot(np.log(self.sigmoid(z))) - (1 - y.T).dot(np.log(1.0 - self.sigmoid(z)))) / m
-        J += lamb/(2.0*m) * theta[1:].T.dot(theta[1:]) # 公式换算，返回第一个属性值
+        J += lamb / (2.0 * m) * theta[1:].T.dot(theta[1:])
         return J[0]
+
 
     def gradientReg(self, theta, lamb, X, y):
         m = X.shape[0]
-        grad = np.zeros_like(theta)
-        z = X.dot(theta.reshape(-1,1))
-        grad = (1.0 / m) * X.T.dot((self.sigmoid(z) - y)) + (lamb / m) * theta.reshape(-1,1)
-        grad[0] -= (lamb/m) * theta[0]
+        z = X.dot(theta.reshape(-1, 1))
+        #
+        grad = (1.0 / m) * X.T.dot((self.sigmoid(z) - y)) + (lamb / m) * theta.reshape(-1, 1)
+        grad[0] -= (lamb / m) * theta[0]
         return grad.flatten()
 
 
 
     def loadData(self, path):
-        path = os.getcwd() + path # 获取到当前工作目录
+        path = os.getcwd() + path  
         data = np.loadtxt(path, dtype=float, delimiter=',')
-        X = data[:, 0:2] # 点 根据数据集的不同而不同
-        y = data[:, 2] # 已经分类好的label
+        X = data[:, 0:2]  
+        y = data[:, 2]  
         return X, y
 
+ 
     def plotScatter(self, path):
         X, y = self.loadData(path)
-        y = y.astype(int) # 确保label是int型的0或1
+        y = y.astype(int) 
         neg = (y == 0)
         pos = (y == 1)
         print(type(neg))
-        lable1 = plt.scatter(X[neg, 0], X[neg, 1], marker='^', c='g', alpha=0.7) # 绘制散点图，先通过上面的两行分类成neg和pos，然后再
+        lable1 = plt.scatter(X[neg, 0], X[neg, 1], marker='^', c='g', alpha=0.7) 
         lable2 = plt.scatter(X[pos, 0], X[pos, 1], marker='v', c='r', alpha=0.7)
         plt.legend((lable1, lable2), ('pass', 'not pass'))
         plt.xlabel('exam 1')
@@ -76,9 +82,10 @@ class LogisticRegression(object):
         self.plotScatter(path)
         X, y = self.loadData(path)
         y.astype(int)
-        boundary_xs = np.array([np.min(X[:, 0]), np.max(X[:, 0])]) # 该参数为第一列数据的[最小值,最大值]的数组
-        boundary_ys = (-1. / theta[2]) * (theta[0] + theta[1] * boundary_xs)
+        boundary_xs = np.array([np.min(X[:, 0]), np.max(X[:, 0])])  
+        boundary_ys = (-1. / theta[2]) * (theta[0] + theta[1] * boundary_xs)  
         plt.plot(boundary_xs, boundary_ys)
+
 
     def decisionBoundaryReg(self, theta, X, y, lamb=0.):
         theta, mincost = self.optimizeRegularizedTheta(theta, X, y, lamb)
@@ -95,9 +102,10 @@ class LogisticRegression(object):
         plt.contour(xvals, yvals, zvals, [0])
         plt.title("Decision Boundary with Lambda = %d" % lamb)
 
+
     def optimizeRegularizedTheta(self, theta, X, y, lamb=0.):
         result = op.minimize(self.costFunctionReg, theta, args=(lamb, X, y), method='BFGS',
-                            options={"maxiter": 500, "disp": False})
+                             options={"maxiter": 500, "disp": False})
         return np.array([result.x]), result.fun
 
     def subplotDecisionBoundary(self, path, theta, X, y):
@@ -119,7 +127,7 @@ class LogisticRegression(object):
     def LogisticReg(self, path):
         X, y = self.loadData(path)
         m, n = X.shape
-        X = np.c_[np.ones((m, 1)), X]  # add one col
+        X = np.c_[np.ones((m, 1)), X] 
         y = np.c_[y]
         X = self.mapFeature(X[:, 1], X[:, 2])
         initial_theta = np.zeros(X.shape[1])
